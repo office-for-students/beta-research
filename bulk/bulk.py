522790a3c04877bfb15b9075c1cfbcc1184c5c77
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-09-27 15:56:22 +0100

    This works can bulk load 50 docs

--- bulk/bulk.py
+++ bulk/bulk.py
@@ -12,15 +12,15 @@ print(collection_link)
 sproc_link = collection_link + "/sprocs/spBulk"
 
 
-def create_cosmos_entity(id, test):
-    return {"id": id, "Test": test}
+def create_cosmos_entity(id):
+    return {"id": id, "test": "1"}
 
 
 new_docs = []
 counter = 0
 while counter < 50:
-    new_docs.append(create_cosmos_entity(str(uuid.uuid4()), counter))
+    new_docs.append(create_cosmos_entity(str(uuid.uuid4())))
     counter += 1
 
-options = {"partitionKey": "id"}
+options = {"partitionKey": "1"}
 client.ExecuteStoredProcedure(sproc_link, [new_docs], options)
